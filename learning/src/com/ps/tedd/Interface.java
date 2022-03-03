cd022228883d7249e074a3841055511d073775c0
kishore7403 <kishore7403@gmail.com>
2022-03-03 22:22:56 +0530

    learnt interfaces

---
+++ learning/src/com/ps/tedd/Interface.java
@@ -0,0 +1,29 @@
+package com.ps.tedd;
+
+interface Runnable{
+    public void run();
+    default void summary(){
+        System.out.println("Summary");
+    }
+}
+class Run implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("person is running");
+    }
+
+//    @Override
+//    public void summary() {
+//        System.out.println("he");
+//    }
+}
+
+
+public class Interface {
+    public static void main(String[] args) {
+        Run runner=new Run();
+        runner.run();
+        runner.summary();
+    }
+
+}
