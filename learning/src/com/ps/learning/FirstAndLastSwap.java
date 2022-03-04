d8fd4177e530b23b1a1355f2cd68ef6d84cab277
kishore7403 <kishore7403@gmail.com>
2022-03-04 13:07:35 +0530

    String index Swapper

---
+++ learning/src/com/ps/learning/FirstAndLastSwap.java
@@ -0,0 +1,16 @@
+package com.ps.learning;
+
+public class FirstAndLastSwap {
+    public static void main(String[] args) {
+        System.out.println("a    =>"+swapper("a"));
+        System.out.println("ab   =>"+swapper("ab"));
+        System.out.println("abc  =>"+swapper("abc"));
+        System.out.println("abcd =>"+swapper("abcd"));
+    }
+
+    public static String swapper(String s){
+
+        return (s.length()==1) ? s : s.charAt(s.length()-1) + s.substring(1, s.length() - 1) + s.charAt(0);
+
+    }
+}
