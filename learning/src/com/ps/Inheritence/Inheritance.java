9ea4f15b32e353399a15d35080bb6b348a477723
kishore7403 <kishore7403@gmail.com>
2022-03-04 10:11:44 +0530

    learnt Inheritance

--- learning/src/com/ps/Inheritence/Inheritance.java
+++ learning/src/com/ps/Inheritence/Inheritance.java
@@ -1,11 +1,15 @@
 package com.ps.Inheritence;
 class A{
     A(){
+
         System.out.println("In a");
     }
     void info(){
         System.out.println("hello A");
     }
+//    void print(){
+//        System.out.println("hi A");
+//    }
 }
 class B extends A{
     B(){
@@ -15,17 +19,26 @@ class B extends A{
         System.out.println("Hello B");
     }
     void print(){
-        System.out.println("hi");
+        System.out.println("hi B");
     }
 }
 class C extends B{
     C(){
         System.out.println("In c");
     }
+    void info (){
+        System.out.println("Hello C");
+    }
+    void print(){
+        System.out.println("hi C");
+    }
 }
 public class Inheritance {
     public static void main(String[] args) {
         A b= new B();
-        b.info();               //runtime polymorphism (method overriding) dynamic method dispatch
+        b.info();       //runtime polymorphism (method overriding) dynamic method dispatch
+//        b.print();
+//        B a= new A();
+
     }
 }
