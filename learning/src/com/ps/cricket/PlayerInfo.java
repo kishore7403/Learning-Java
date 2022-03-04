7849dac6611361681b032f4e429db347f9ee2351
kishore7403 <kishore7403@gmail.com>
2022-03-04 10:13:27 +0530

    inheritance

--- learning/src/com/ps/cricket/PlayerInfo.java
+++ learning/src/com/ps/cricket/PlayerInfo.java
@@ -1,10 +1,22 @@
 package com.ps.cricket;
 
 public class PlayerInfo {
+    String name;
+    private int runscored;
+    protected int stamina;
+    public int ranking;
 
-    public void play(){
-
+    void printName(){
+        System.out.println("name");
     }
 
-
+    private void printRunsScored(){
+        System.out.println("runs");
+    }
+    protected void printStamina(){
+        System.out.println("stamina");
+    }
+    public void printRanking(){
+        System.out.println("ranking");
+    }
 }
