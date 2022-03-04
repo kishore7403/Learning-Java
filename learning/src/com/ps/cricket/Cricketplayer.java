8d5da7643033a9c0092b06c1477c6782501d40da
kishore7403 <kishore7403@gmail.com>
2022-03-04 10:12:20 +0530

    Access modifiers

--- learning/src/com/ps/cricket/Cricketplayer.java
+++ learning/src/com/ps/cricket/Cricketplayer.java
@@ -1,13 +1,8 @@
 package com.ps.cricket;
 
-public class Cricketplayer extends PlayerInfo {
+public class Cricketplayer extends PlayerInfo{
     public static void main(String[] args) {
-//        PlayerInfo sachin=new PlayerInfo();
-//        sachin.setName("Sachin");
-//        sachin.setScore(100);
-//        System.out.println(sachin.getName());
-//        System.out.println(sachin.getScore());
-//        sachin.name="dhoni";
-//        System.out.println(sachin.name);
+        PlayerInfo c1 =new PlayerInfo();
+
     }
 }
--- learning/src/com/ps/football/FootballPlayer.java
+++ learning/src/com/ps/football/FootballPlayer.java
@@ -3,13 +3,10 @@ package com.ps.football;
 
 import com.ps.cricket.PlayerInfo;
 
-public class FootballPlayer {
+public class FootballPlayer extends PlayerInfo  {
     public static void main(String[] args) {
-//        PlayerInfo messi=new PlayerInfo();
-//        messi.setName("Messi");
-//        messi.setScore(25);
-//        System.out.println(messi.getName());
-//        System.out.println(messi.getScore());
+        PlayerInfo f1 =new PlayerInfo();
+
 
     }
 }
